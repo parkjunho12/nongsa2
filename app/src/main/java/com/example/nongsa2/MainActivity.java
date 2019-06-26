dc8013f3e71d7c2a7d3a1764da0217f040a9bc68
Junho <ghdlwnsgh12@naver.com>
2019-06-26 16:26:08 +0900

    프레그먼트

--- app/src/main/java/com/example/nongsa2/MainActivity.java
+++ app/src/main/java/com/example/nongsa2/MainActivity.java
@@ -1,33 +1,48 @@
 package com.example.nongsa2;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
+    FragmentManager fm = getSupportFragmentManager();
+    Fragment fragment = fm.findFragmentById(R.id.container);
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    fragment = new PlusOneFragment();
+
+                    fm.beginTransaction().replace(R.id.container,fragment).commit();
+                    Log.d("ddd","MonthFragment");
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
+            }
+            if(fragment!=null) {
+                Log.d("ddd","setfm");
+                FragmentManager fm = getSupportFragmentManager();
+                fm.beginTransaction().replace(R.id.container, fragment).commit();
             }
             return false;
         }
@@ -39,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
+
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
