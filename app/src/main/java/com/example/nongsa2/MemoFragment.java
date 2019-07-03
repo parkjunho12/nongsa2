863773eca8ca8c2ac9de63bfa1842631409e9533
yhs <laugh4mile>
2019-07-04 01:24:56 +0900

    정보등록 다되감

--- app/src/main/java/com/example/nongsa2/MemoFragment.java
+++ app/src/main/java/com/example/nongsa2/MemoFragment.java
@@ -79,9 +79,11 @@ public class MemoFragment extends Fragment implements MainActivity.OnBackPressed
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-    ArrayAdapter<CharSequence> adspin1, adspin2; //어댑터를 선언했습니다. adspint1(서울,인천..) adspin2(강남구,강서구..)
+    ArrayAdapter<CharSequence> adspin1, adspin2, adspin3, adspin4; //어댑터를 선언했습니다. adspint1(서울,인천..) adspin2(강남구,강서구..)
     String choice_do="";
     String choice_se="";//검색시 선택된 매세지를 띄우기 위한 선언하였습니다. 그냥 선언안하고 인자로 넘기셔도 됩니다.
+    String choice_deal_type="";
+    String choice_gubun="";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -93,9 +95,8 @@ public class MemoFragment extends Fragment implements MainActivity.OnBackPressed
         final EditText edit_OWNER_NM = (EditText) view.findViewById(R.id.OWNER_NM);
         final EditText edit_OWNER_CONTACT = (EditText) view.findViewById(R.id.OWNER_CONTACT);
         final EditText edit_DEAL_AMAUNT = (EditText) view.findViewById(R.id.DEAL_AMAUNT);
+        final EditText edit_DEAL_BIGO = (EditText) view.findViewById(R.id.DEAL_BIGO);
 
-        final Spinner spinner1 = (Spinner)view.findViewById(R.id.spinner1);
-        final String text1 = spinner1.getSelectedItem().toString(); // 도. 스피너에서 선택된 항목의 값을 text1로 받음
 
 //        final Spinner spinner2 = (Spinner)view.findViewById(R.id.spinner2);
 //        final String text2 = spinner2.getSelectedItem().toString(); // 시군. 스피너에서 선택된 항목의 값을 text2로 받음
@@ -108,6 +109,7 @@ public class MemoFragment extends Fragment implements MainActivity.OnBackPressed
                 String OWNER_NM =edit_OWNER_NM.getText().toString();
                 String OWNER_CONTACT =edit_OWNER_CONTACT.getText().toString();
                 String DEAL_AMAUNT =edit_DEAL_AMAUNT.getText().toString();
+                String DEAL_BIGO =edit_DEAL_BIGO.getText().toString();
 
 
                 Response.Listener<String> responseListener =new Response.Listener<String>() {
@@ -145,19 +147,54 @@ public class MemoFragment extends Fragment implements MainActivity.OnBackPressed
                         }
                     }
                 };
-                MemoRequest memoRequest =new MemoRequest(text1,ADDR,ADDR, OWNER_NM, OWNER_CONTACT, DEAL_AMAUNT,responseListener);
+                MemoRequest memoRequest =new MemoRequest(choice_do,choice_se,ADDR,OWNER_NM,OWNER_CONTACT,DEAL_AMAUNT,choice_deal_type,DEAL_BIGO,choice_gubun,responseListener);
                 RequestQueue queue= Volley.newRequestQueue(getActivity().getApplicationContext());
                 queue.add(memoRequest);
             }
         });
 
-        ///////////////////////////////////
-        ////////////* 뒤로가기*////////////
-        ///////////////////////////////////
+
+
         final Spinner spin1 = (Spinner)view.findViewById(R.id.spinner1);
         final Spinner spin2 = (Spinner)view.findViewById(R.id.spinner2);
+
+        final Spinner spin3 = (Spinner)view.findViewById(R.id.DEAL_TYPE);
+
+        adspin3 = ArrayAdapter.createFromResource(getContext(),R.array.maemae, android.R.layout.simple_spinner_dropdown_item);
+        spin3.setAdapter(adspin3);
+        spin3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                choice_deal_type = adspin3.getItem(i).toString();//선택된 값을 넣습니다.
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        final Spinner spin4 = (Spinner)view.findViewById(R.id.GUBUN);
+
+        adspin4 = ArrayAdapter.createFromResource(getContext(),R.array.gubun, android.R.layout.simple_spinner_dropdown_item);
+        spin4.setAdapter(adspin4);
+        spin4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                choice_gubun = adspin4.getItem(i).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
 //        Button btn_refresh = (Button)findViewById(R.id.btn_refresh);//xml과 class에 변수들을 연결해줍니다. final를 사용한 이유는 spin2가 함수안에서 사용되기 때문에 코딩전체로 선언한 것입니다.
-        adspin1 = ArrayAdapter.createFromResource(getContext(), R.array.sido, android.R.layout.simple_spinner_dropdown_item);//첫번째 어댑터에 값을 넣습니다. this=는 현재class를 의미합니다. R.array.spinner_do는 이곳에 도시를 다 쓸 경우 코딩이 길어지기 때문에 value->string.xml에 따로 String값들을 선언해두었습니다. //R.layout.simple_~~~는 안드로이드에서 기본제공하는 spinner 모양입니다. 다른것도 있는데 비슷합니다. adspin1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);//이부분이 정확히 말로 설명을 못하겠습니다. 아무튼 필요합니다. 헤헤 고수분들 도와주세요.
+        adspin1 = ArrayAdapter.createFromResource(getContext(), R.array.sido, android.R.layout.simple_spinner_dropdown_item);//첫번째 어댑터에 값을 넣습니다. this=는 현재class를 의미합니다.
+        // R.array.spinner_do는 이곳에 도시를 다 쓸 경우 코딩이 길어지기 때문에 value->string.xml에 따로 String값들을 선언해두었습니다.
+        // R.layout.simple_~~~는 안드로이드에서 기본제공하는 spinner 모양입니다. 다른것도 있는데 비슷합니다.
+        // adspin1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);//이부분이 정확히 말로 설명을 못하겠습니다. 아무튼 필요합니다. 헤헤 고수분들 도와주세요.
         spin1.setAdapter(adspin1);
 //어댑터에 값들을 spinner에 넣습니다. 여기까지 하시면 첫번째 spinner에 값들이 들어갈 것입니다.
         spin1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {//첫번째 spinner 클릭시 이벤트 발생입니다. setO 정도까지 치시면 자동완성됩니다. 뒤에도 마찬가지입니다.
--- app/src/main/java/com/example/nongsa2/MemoRequest.java
+++ app/src/main/java/com/example/nongsa2/MemoRequest.java
@@ -12,7 +12,7 @@ public class MemoRequest extends StringRequest {
     final static private String URL ="http://dbwo4011.cafe24.com/migration/memo.php";
     private Map<String, String> parameters;
 
-    public MemoRequest(String SIDO_NM, String SIGUN_NM, String ADDR, String OWNER_NM, String OWNER_CONTACT, String DEAL_AMAUNT,Response.Listener<String> listener) {
+    public MemoRequest(String SIDO_NM, String SIGUN_NM, String ADDR, String OWNER_NM, String OWNER_CONTACT, String DEAL_AMAUNT, String DEAL_TYPE, String DEAL_BIGO, String GUBUN, Response.Listener<String> listener) {
         super(Method.POST, URL, listener, null);
 
         parameters=new HashMap<>();
@@ -22,6 +22,9 @@ public class MemoRequest extends StringRequest {
         parameters.put("OWNER_NM",OWNER_NM);
         parameters.put("OWNER_CONTACT",OWNER_CONTACT);
         parameters.put("DEAL_AMAUNT",DEAL_AMAUNT);
+        parameters.put("DEAL_TYPE",DEAL_TYPE);
+        parameters.put("DEAL_BIGO",DEAL_BIGO);
+        parameters.put("GUBUN",GUBUN);
 
         Log.e(this.getClass().getName(),"정보 등록!");
         Log.e(this.getClass().getName(),"시도명!"+SIDO_NM);
@@ -29,7 +32,7 @@ public class MemoRequest extends StringRequest {
         Log.e(this.getClass().getName(),"상세주소!"+SIDO_NM);
         Log.e(this.getClass().getName(),"소유주!"+OWNER_NM);
         Log.e(this.getClass().getName(),"소유주 전화번호!"+OWNER_CONTACT);
-        Log.e(this.getClass().getName(),"희망거래가!"+DEAL_AMAUNT);
+        Log.e(this.getClass().getName(),"희망거래가!"+DEAL_TYPE);
     }
     public Map<String, String> getParams() { return parameters; }
 }
