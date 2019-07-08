3c40d03bba2f8113bb52fd73b75e04b30fb27f21
yhs <laugh4mile>
2019-07-09 02:49:14 +0900

    갤러리에 있는 사진 불러와서 이미지뷰에 띄움

--- app/src/main/java/com/example/nongsa2/MemoFragment.java
+++ app/src/main/java/com/example/nongsa2/MemoFragment.java
@@ -83,7 +83,7 @@ public class MemoFragment extends Fragment implements MainActivity.OnBackPressed
         // Required empty public constructor
 
         edit_ADDR = null;
-    edit_OWNER_NM=null ;
+        edit_OWNER_NM=null ;
         edit_OWNER_CONTACT=null ;
         edit_DEAL_AMOUNT =null;
         edit_DEAL_BIGO=null;
@@ -129,11 +129,137 @@ public class MemoFragment extends Fragment implements MainActivity.OnBackPressed
     EditText edit_OWNER_CONTACT ;
     EditText edit_DEAL_AMOUNT ;
     EditText edit_DEAL_BIGO;
+
+    private static final String TAG = "blackjin";
+    private Boolean isPermission = true;
+    private static final int PICK_FROM_ALBUM = 1;
+    private static final int PICK_FROM_CAMERA = 2;
+    private File tempFile;
+
+    private void tedPermission() {
+
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                // 권한 요청 성공
+                isPermission = true;
+
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                // 권한 요청 실패
+                isPermission = false;
+
+            }
+        };
+
+        TedPermission.with(getContext())
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage(getResources().getString(R.string.permission_2))
+                .setDeniedMessage(getResources().getString(R.string.permission_1))
+                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+                .check();
+
+    }
+    private void goToAlbum() {
+
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            Toast.makeText(getContext(), "취소 되었습니다.", Toast.LENGTH_SHORT).show();
+
+            if(tempFile != null) {
+                if (tempFile.exists()) {
+                    if (tempFile.delete()) {
+                        Log.e(TAG, tempFile.getAbsolutePath() + " 삭제 성공");
+                        tempFile = null;
+                    }
+                }
+            }
+
+            return;
+        }
+
+        if (requestCode == PICK_FROM_ALBUM) {
+
+            Uri photoUri = data.getData();
+            Log.d(TAG, "PICK_FROM_ALBUM photoUri : " + photoUri);
+
+            Cursor cursor = null;
+
+            try {
+
+                /*
+                 *  Uri 스키마를
+                 *  content:/// 에서 file:/// 로  변경한다.
+                 */
+                String[] proj = { MediaStore.Images.Media.DATA };
+
+                assert photoUri != null;
+                cursor = getActivity().getContentResolver().query(photoUri, proj, null, null, null);
+
+                assert cursor != null;
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+                cursor.moveToFirst();
+
+                tempFile = new File(cursor.getString(column_index));
+
+                Log.d(TAG, "tempFile Uri : " + Uri.fromFile(tempFile));
+
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+
+            setImage();
+
+        } else if (requestCode == PICK_FROM_CAMERA) {
+
+            setImage();
+
+        }
+    }
+
+    private void setImage() {
+
+        ImageView imageView = getView().findViewById(R.id.imageView);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        Bitmap originalBm = BitmapFactory.decodeFile(tempFile.getAbsolutePath(), options);
+
+        imageView.setImageBitmap(originalBm);
+
+        tempFile = null;
+    }
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =inflater.inflate(R.layout.fragment_memo, container, false);
+
+        tedPermission();
+
+        Button mGallery_btn = (Button) view.findViewById(R.id.btnGallery);
+        mGallery_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 권한 허용에 동의하지 않았을 경우 토스트를 띄웁니다.
+                if(isPermission) goToAlbum();
+                else Toast.makeText(view.getContext(), getResources().getString(R.string.permission_2), Toast.LENGTH_LONG).show();
+            }
+        });
+
 
        edit_ADDR = (EditText) view.findViewById(R.id.ADDR);
 
